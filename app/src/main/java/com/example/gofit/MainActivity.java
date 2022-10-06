4455d676796a1b3eeca89f2db80d6b633f5a9655
aramkeshishyan <72706358+aramkeshishyan@users.noreply.github.com>
2022-10-06 15:51:01 -0700

    Testing features

--- app/src/main/java/com/example/gofit/MainActivity.java
+++ app/src/main/java/com/example/gofit/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+    }
+
+    public void onBtnClick(View view){
+
+        TextView txtHello = findViewById(R.id.txtMessage);
+        EditText edtTxtName = findViewById(R.id.edtTxtName);
+        txtHello.setText("Hello " + edtTxtName.getText().toString());
     }
 }
