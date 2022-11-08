7901aeaa6ca0d7a082789f78333cb4a2abbe9ae5
aramkeshishyan <72706358+aramkeshishyan@users.noreply.github.com>
2022-11-08 14:33:38 -0800

    added logout functionality

--- app/src/main/java/com/example/gofit/UserProfile.java
+++ app/src/main/java/com/example/gofit/UserProfile.java
@@ -2,13 +2,31 @@ package com.example.gofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserProfile extends AppCompatActivity {
+
+    private ImageButton logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        logout = (ImageButton) findViewById(R.id.logOutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserProfile.this, MainActivity.class));
+                Toast.makeText(UserProfile.this, "Logged out successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
