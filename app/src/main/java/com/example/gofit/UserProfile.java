c5513718a2a19e456575ea2ce2984d695036f179
aramkeshishyan <72706358+aramkeshishyan@users.noreply.github.com>
2022-11-08 15:17:12 -0800

    users name now displaying in the user profile page

--- app/src/main/java/com/example/gofit/UserProfile.java
+++ app/src/main/java/com/example/gofit/UserProfile.java
@@ -1,24 +1,40 @@
 package com.example.gofit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class UserProfile extends AppCompatActivity {
 
     private ImageButton logout;
+
+    //to reference user info from database
+    private FirebaseUser user;
+    private DatabaseReference reference; //example, reference the "Users" collection
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        //logout button functionality
         logout = (ImageButton) findViewById(R.id.logOutBtn);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +44,32 @@ public class UserProfile extends AppCompatActivity {
                 Toast.makeText(UserProfile.this, "Logged out successfully!", Toast.LENGTH_SHORT).show();
             }
         });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        //get users fullname to display under the avatar
+        final TextView fullNameTextView = (TextView) findViewById(R.id.userFullName);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Creating a User.java object
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String fullName = userProfile.fullName;
+                    fullNameTextView.setText(fullName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserProfile.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        //////////////////////////////////////////////////////////
     }
 }
