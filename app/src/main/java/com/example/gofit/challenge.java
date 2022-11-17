d5001d332c58ca100fcbb17c7e6430276b3ca28d
Felix Moreno <felixmoreno981@gmail.com>
2022-11-16 23:53:38 -0800

    added challenge page layout

---
+++ app/src/main/java/com/example/gofit/challenge.java
@@ -0,0 +1,55 @@
+package com.example.gofit;
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+
+
+public class challenge extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.challenege_page);
+        Toolbar tool_bar = findViewById(R.id.my_tool_bar);
+        setSupportActionBar(tool_bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.profile_image);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(this, "menu_one", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}
+
