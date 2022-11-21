6fd26fb225d20d22f7d9bd7de0e78a9cdbd4498b
aramkeshishyan <72706358+aramkeshishyan@users.noreply.github.com>
2022-11-20 20:39:54 -0800

    added activities

--- app/src/main/java/com/example/gofit/HomePage.java
+++ app/src/main/java/com/example/gofit/HomePage.java
@@ -1,16 +1,21 @@
 package com.example.gofit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView profile_pic;
-
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +24,31 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
         profile_pic = (ImageView) findViewById(R.id.profile_pic);
         profile_pic.setOnClickListener(this);
 
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        ///
+                        return true;
+                    case R.id.exercise:
+                        startActivity(new Intent(HomePage.this, ExercisePage.class));
+                        return true;
+                    case R.id.nutrition:
+                        startActivity(new Intent(HomePage.this, NutritionPage.class));
+                        return true;
+                    case R.id.challenges:
+                        startActivity(new Intent(HomePage.this, challenge.class));
+                        return true;
+                    default: return true;
+                }
 
+            }
+        });
 
 
 
