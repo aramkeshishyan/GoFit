adf187c1ea046f8b30b48cd808de7cb5e01de005
HanyAkbari <99519077+HanyAkbari@users.noreply.github.com>
2022-11-27 16:25:52 -0800

    Fragments created. Home page layout moved to first fragment

---
+++ app/src/main/java/com/example/gofit/LemonChickenPastaSymflowerTest.java
@@ -0,0 +1,13 @@
+package com.example.gofit;
+
+import android.os.Bundle;
+import org.junit.*;
+
+public class LemonChickenPastaSymflowerTest {
+	@Test
+	public void onCreate1() {
+		LemonChickenPasta l = null; // TODO This is a fallback value due to incomplete analysis.
+		Bundle savedInstanceState = null; // TODO This is a fallback value due to incomplete analysis.
+		l.onCreate(savedInstanceState);
+	}
+}
