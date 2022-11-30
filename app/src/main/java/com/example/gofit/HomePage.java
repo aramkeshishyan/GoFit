800e85d33b7658f557193a8ee16718507b82caf1
Steven G <steven.guzman.91@my.csun.edu>
2022-11-29 16:32:56 -0800

    Added textview to the top bar on Homepage

--- app/src/main/java/com/example/gofit/HomePage.java
+++ app/src/main/java/com/example/gofit/HomePage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -20,7 +21,6 @@ public class HomePage extends AppCompatActivity implements NavigationBarView.OnI
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-
         profile_pic = (ImageView) findViewById(R.id.profile_pic);
         profile_pic.setOnClickListener(this);
 
@@ -37,18 +37,23 @@ public class HomePage extends AppCompatActivity implements NavigationBarView.OnI
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+        TextView set_title = findViewById(R.id.title_text);
         switch (item.getItemId()) {
             case R.id.home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.Fragment, firstFragment).commit();
+                set_title.setText("Home");
                 return true;
             case R.id.exercise:
                 getSupportFragmentManager().beginTransaction().replace(R.id.Fragment, secondFragment).commit();
+                set_title.setText("Exercise");
                 return true;
             case R.id.nutrition:
                 getSupportFragmentManager().beginTransaction().replace(R.id.Fragment, thirdFragment).commit();
+                set_title.setText("Nutrition");
                 return true;
             case R.id.challenges:
                 getSupportFragmentManager().beginTransaction().replace(R.id.Fragment, fourthFragment).commit();
+                set_title.setText("Challenges");
                 return true;
         }
         return false;
