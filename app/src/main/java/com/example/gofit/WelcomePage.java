177afedccbd104580fc739bcbe2035ef798bea0e
Alisha <56981295+alishcodes@users.noreply.github.com>
2023-02-05 16:07:20 -0800

    Made minor changes: Fixed WelcomePage java file Deleted unused java file Added id to button in welcome page

--- app/src/main/java/com/example/gofit/WelcomePage.java
+++ app/src/main/java/com/example/gofit/WelcomePage.java
@@ -1,7 +1,21 @@
 package com.example.gofit;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
-public class WelcomePage extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomePage extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            //case R.id.signInBtn:
+        }
+    }
 }
 
