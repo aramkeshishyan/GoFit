7d17059a3ebcd544111c420e57c9d01aeaebb7f4
aramkeshishyan <72706358+aramkeshishyan@users.noreply.github.com>
2023-02-18 21:11:22 -0800

    added survey1 and survey2 activities to get user info

--- app/src/main/java/com/example/gofit/Survey1.java
+++ app/src/main/java/com/example/gofit/Survey1.java
@@ -2,13 +2,33 @@ package com.example.gofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Survey1 extends AppCompatActivity {
+public class Survey1 extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey1);
+
+
+        btnContinue = (Button) findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnContinue:
+                startActivity(new Intent(Survey1.this, Survey2.class));
+                break;
+            case R.id.activityBanner:
+        }
     }
 }
--- app/src/main/java/com/example/gofit/Survey2.java
+++ app/src/main/java/com/example/gofit/Survey2.java
@@ -2,13 +2,31 @@ package com.example.gofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Survey2 extends AppCompatActivity {
+public class Survey2 extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey2);
+
+        btnContinue = (Button) findViewById(R.id.btnContinue);
+        btnContinue.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnContinue:
+                startActivity(new Intent(Survey2.this, HomePage.class));
+                break;
+        }
+
     }
 }
