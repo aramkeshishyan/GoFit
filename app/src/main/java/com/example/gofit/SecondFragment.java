f255e6b10eb96a200638c91974fee31b266a7288
stevenguzman91 <92278296+stevenguzman91@users.noreply.github.com>
2023-02-19 23:03:25 -0800

    Spinner and Search added to Exercise Page

--- app/src/main/java/com/example/gofit/SecondFragment.java
+++ app/src/main/java/com/example/gofit/SecondFragment.java
@@ -10,14 +10,25 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class SecondFragment extends Fragment {
 
     private RecyclerView exercise;
+    private Spinner exercise_spinner;
+    private String[] exercise_categories = {"All", "Core", "Upper", "Lower"};
+    private SearchView exercise_searchview;
+    private ArrayList<RecView_Item> exerciseList;
     public SecondFragment(){
         // require a empty public constructor
     }
@@ -31,13 +42,13 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ArrayList<RecView_Item> exerciseList = new ArrayList<>();
-        exerciseList.add(new RecView_Item("Crunches", "Abdominal", "https://betterme.world/articles/wp-content/uploads/2020/10/How-Many-Calories-Do-You-Burn-Doing-Crunches.jpg"));
-        exerciseList.add(new RecView_Item("Squats", "Quadriceps, Gluteus, Hips, Abdominals", "https://experiencelife.lifetime.life/wp-content/uploads/2021/02/Squat-1-1280x720.jpg"));
-        exerciseList.add(new RecView_Item("Lunges", "Quadriceps, Hamstrings, Gluteus, Hips", "https://post.healthline.com/wp-content/uploads/2020/09/11159-Mix_things_up_with_this_lunge_and_bicep_curl_compound_move_732x549-thumbnail-732x549.jpg"));
-        exerciseList.add(new RecView_Item("Push-up", "Pectorals, Triceps", "https://blog.nasm.org/hubfs/power-pushups.jpg"));
-        exerciseList.add(new RecView_Item("Leg Raise", "Hips", "https://cathe.com/wp-content/uploads/2019/10/shutterstock_363953936.jpg"));
-        exerciseList.add(new RecView_Item("Forearm Plank", "Abdominals", "https://www.wellandgood.com/wp-content/uploads/2019/03/GettyImages-855913544.jpg"));
+        exerciseList = new ArrayList<>();
+        exerciseList.add(new RecView_Item("Crunches", "Core", "https://betterme.world/articles/wp-content/uploads/2020/10/How-Many-Calories-Do-You-Burn-Doing-Crunches.jpg"));
+        exerciseList.add(new RecView_Item("Squats", "Lower", "https://experiencelife.lifetime.life/wp-content/uploads/2021/02/Squat-1-1280x720.jpg"));
+        exerciseList.add(new RecView_Item("Lunges", "Lower", "https://post.healthline.com/wp-content/uploads/2020/09/11159-Mix_things_up_with_this_lunge_and_bicep_curl_compound_move_732x549-thumbnail-732x549.jpg"));
+        exerciseList.add(new RecView_Item("Push-up", "Upper", "https://blog.nasm.org/hubfs/power-pushups.jpg"));
+        exerciseList.add(new RecView_Item("Leg Raise", "Lower", "https://cathe.com/wp-content/uploads/2019/10/shutterstock_363953936.jpg"));
+        exerciseList.add(new RecView_Item("Forearm Plank", "Core", "https://www.wellandgood.com/wp-content/uploads/2019/03/GettyImages-855913544.jpg"));
         ExerciseRecViewAdapter exerciseAdapter = new ExerciseRecViewAdapter(getContext(),exerciseList);
         exerciseAdapter.setExercises(exerciseList);
 
@@ -48,5 +59,78 @@ public class SecondFragment extends Fragment {
 
         exerciseAdapter.notifyDataSetChanged();
 
+        exercise_spinner = view.findViewById(R.id.exercise_spinner);
+        exercise_spinner.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, exercise_categories));
+
+        //Spinner selection events
+        exercise_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(exercise_categories[i] == "All"){
+                    //set all exercises in RecView using exerciseList
+                    ExerciseRecViewAdapter exerciseAdapter = new ExerciseRecViewAdapter(getContext(),exerciseList);
+                    exerciseAdapter.setExercises(exerciseList);
+                    exercise.setAdapter(exerciseAdapter);
+                }
+                else {
+                    //create new Arraylist
+                    ArrayList<RecView_Item> newExerciseList = new ArrayList<>();
+                    //for every item in exerciseList
+                    for (RecView_Item checkExerciseList : exerciseList) {
+                        //comparing if strings match with category selected
+                        if (checkExerciseList.getItem_description() == exercise_categories[i]) {
+                            //add item into new array list if category match
+                            newExerciseList.add(checkExerciseList);
+                        }
+                    }
+                    //set new list into RecView
+                    ExerciseRecViewAdapter exerciseAdapter = new ExerciseRecViewAdapter(getContext(), newExerciseList);
+                    exerciseAdapter.setExercises(newExerciseList);
+                    exercise.setAdapter(exerciseAdapter);
+
+                    //Toast.makeText(getContext().getApplicationContext(), exercise_categories[i], Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //Search Filtering
+        exercise_searchview = view.findViewById(R.id.exercise_search);
+        exercise_searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                filter(s);
+                return false;
+            }
+        });
+    }
+
+    private void filter(String s) {
+        //create new Arraylist
+        ArrayList<RecView_Item> newExerciseList = new ArrayList<>();
+        for (RecView_Item exercise : exerciseList){
+            //checks if entered string matches with the name of the item
+            if (exercise.getItem_name().toLowerCase().contains(s.toLowerCase())){
+                newExerciseList.add(exercise);
+            }
+        }
+        if (newExerciseList.isEmpty()){
+            Toast.makeText(getContext(), "No Exercise found.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            //set new list into RecView
+            ExerciseRecViewAdapter exerciseAdapter = new ExerciseRecViewAdapter(getContext(), newExerciseList);
+            exerciseAdapter.setExercises(newExerciseList);
+            exercise.setAdapter(exerciseAdapter);
+        }
     }
 }
