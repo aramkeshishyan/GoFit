b2d0b535f6408acad95bf826233a6f3aa1c8b7b6
HanyAkbari <99519077+HanyAkbari@users.noreply.github.com>
2023-02-26 01:22:02 -0800

    Setting Page (Part 2)

--- app/src/main/java/com/example/gofit/Settings.java
+++ app/src/main/java/com/example/gofit/Settings.java
@@ -25,6 +25,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import android.app.Activity;
+import android.text.InputType;
 
 public class Settings extends AppCompatActivity {
 
@@ -43,6 +44,67 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        // Add your future code here
+        // Get references to the views
+        ImageView profilePicture = findViewById(R.id.profilePicture);
+        Button uploadButton = findViewById(R.id.uploadButton);
+        TextView nameText = findViewById(R.id.nameText);
+        Button changeNameButton = findViewById(R.id.changeNameButton);
+
+        // Set OnClickListener on the uploadButton
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to open the Gallery or Camera app
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                // Start the activity to select an image
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+            }
+        });
+
+        // Set OnClickListener on the changeNameButton
+        changeNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a dialog to get the new name from the user
+                AlertDialog.Builder builder = new AlertDialog.Builder(Settings.this);
+                builder.setTitle("Enter Your Name");
+
+                final EditText input = new EditText(Settings.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newName = input.getText().toString();
+                        // Update the text of the TextView with the new name
+                        nameText.setText(newName);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    // Handle the result from the Gallery or Camera app
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            // Set the selected image as the profile picture
+            ImageView profilePicture = findViewById(R.id.profilePicture);
+            Uri imageUri = data.getData();
+            profilePicture.setImageURI(imageUri);
+        }
     }
 }
--- src/test/java/com/example/gofit/SettingsSymflowerTest.java
+++ src/test/java/com/example/gofit/SettingsSymflowerTest.java
@@ -1,11 +1,21 @@
 package com.example.gofit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import org.junit.jupiter.api.*;
 
 public class SettingsSymflowerTest {
 	@Test
-	public void onCreate1() {
+	public void onActivityResult1() {
+		Settings s = null; // TODO This is a fallback value due to incomplete analysis.
+		int requestCode = 0; // TODO This is a fallback value due to incomplete analysis.
+		int resultCode = 0; // TODO This is a fallback value due to incomplete analysis.
+		Intent data = null; // TODO This is a fallback value due to incomplete analysis.
+		s.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Test
+	public void onCreate2() {
 		Settings s = null; // TODO This is a fallback value due to incomplete analysis.
 		Bundle savedInstanceState = null; // TODO This is a fallback value due to incomplete analysis.
 		s.onCreate(savedInstanceState);
