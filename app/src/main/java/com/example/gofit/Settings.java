42c84e943a21aed834453d01e2b9ed0780079e98
HanyAkbari <99519077+HanyAkbari@users.noreply.github.com>
2023-02-26 00:50:14 -0800

    Setting Page (Part 1)

--- app/src/main/java/com/example/gofit/Settings.java
+++ app/src/main/java/com/example/gofit/Settings.java
@@ -1,31 +1,48 @@
 package com.example.gofit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import android.app.Activity;
 
-public class Settings extends AppCompatActivity implements View.OnClickListener{
-
-    private ImageButton backBtnSettings;
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        backBtnSettings = findViewById(R.id.backBtnSettings);
-        backBtnSettings.setOnClickListener(this);
+        // Find the ImageButton using its ID
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this, UserProfile.class);
+                startActivity(intent);
+            }
+        });
 
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.backBtnSettings:
-                super.finish();
-                break;
-        }
+        // Add your future code here
     }
 }
---
+++ src/test/java/com/example/gofit/SettingsSymflowerTest.java
@@ -0,0 +1,13 @@
+package com.example.gofit;
+
+import android.os.Bundle;
+import org.junit.jupiter.api.*;
+
+public class SettingsSymflowerTest {
+	@Test
+	public void onCreate1() {
+		Settings s = null; // TODO This is a fallback value due to incomplete analysis.
+		Bundle savedInstanceState = null; // TODO This is a fallback value due to incomplete analysis.
+		s.onCreate(savedInstanceState);
+	}
+}
