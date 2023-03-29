e2af48ab18ae8d28547169ed9e36290c9b3e1cee
stevenguzman91 <92278296+stevenguzman91@users.noreply.github.com>
2023-03-28 19:46:26 -0700

    changes to the first fragment

--- app/src/main/java/com/example/gofit/First_Fragment.java
+++ app/src/main/java/com/example/gofit/First_Fragment.java
@@ -7,47 +7,61 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class First_Fragment extends Fragment{
+import java.util.ArrayList;
 
-    private CardView achievement_button;
+public class First_Fragment extends Fragment implements ExerciseRecViewAdapter.OnNoteListener{
+
+    private RecyclerView exercise, nutrition;
+    private ArrayList<Exercise_Item> exercise_list;
+    private ArrayList<Nutrition_Item> nutrition_list;
     public First_Fragment(){
         // require a empty public constructor
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_first_, container, false);
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        achievement_button = view.findViewById(R.id.achievements_card);
-        achievement_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), AchievementsPage.class));
-            }
-        });
-        /*
-        ImageView profilePic = (ImageView) view.findViewById(R.id.profile_pic);
-        profilePic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(), UserProfile.class));
+        exercise_list = new ArrayList<>();
+        nutrition_list = new ArrayList<>();
 
-            }
-        });
+        exercise_list.add(new Exercise_Item("E1_C", "Crunches", "Core", "Hard", "Test",  "https://betterme.world/articles/wp-content/uploads/2020/10/How-Many-Calories-Do-You-Burn-Doing-Crunches.jpg"));
+        exercise_list.add(new Exercise_Item("E11_L", "Squats", "Legs", "Medium","Test", "https://experiencelife.lifetime.life/wp-content/uploads/2021/02/Squat-1-1280x720.jpg"));
+        exercise_list.add(new Exercise_Item("E3-L", "Lunges", "Legs", "Easy","Test", "https://post.healthline.com/wp-content/uploads/2020/09/11159-Mix_things_up_with_this_lunge_and_bicep_curl_compound_move_732x549-thumbnail-732x549.jpg"));
+        exercise_list.add(new Exercise_Item("E8-C", "Push-up", "Chest", "Hard","Test", "https://blog.nasm.org/hubfs/power-pushups.jpg"));
+        exercise_list.add(new Exercise_Item("E3_C", "Leg Raise", "Core", "Hard","Test", "https://cathe.com/wp-content/uploads/2019/10/shutterstock_363953936.jpg"));
+        exercise_list.add(new Exercise_Item("E5-A", "Plank", "Abs", "Hard","Test", "https://www.wellandgood.com/wp-content/uploads/2019/03/GettyImages-855913544.jpg"));
+        RecommendExerciseAdapter exerciseAdapter = new RecommendExerciseAdapter(getContext(), exercise_list, this::onNoteClick);
+        exerciseAdapter.setExercises(exercise_list);
 
-         */
-
+        exercise = view.findViewById(R.id.rec_exercise_recview);
+        exercise.setAdapter(exerciseAdapter);
+        exercise.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+    }
+    @Override
+    public void onNoteClick(int position) {
+        Intent intent = new Intent(getContext(), ExerciseActivity.class);
+        intent.putExtra("ex_id", exercise_list.get(position).getItem_id());
+        intent.putExtra("ex_title", exercise_list.get(position).getItem_name());
+        intent.putExtra("ex_group", exercise_list.get(position).getItem_mGroup());
+        intent.putExtra("ex_level", exercise_list.get(position).getItem_level());
+        intent.putExtra("ex_description", exercise_list.get(position).getItem_description());
+        intent.putExtra("ex_imageUrl", exercise_list.get(position).getItem_image());
+        startActivity(intent);
+        Toast.makeText(getContext(), exercise_list.get(position).getItem_name() + " has been pressed.", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/gofit/RecommendExerciseAdapter.java
@@ -0,0 +1,69 @@
+package com.example.gofit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class RecommendExerciseAdapter extends RecyclerView.Adapter<RecommendExerciseAdapter.ViewHolder> {
+    private ArrayList<Exercise_Item> exerciseArrayList;
+    private OnNoteListener onNoteListener;
+    private Context exercise_context;
+
+    public RecommendExerciseAdapter(Context exercise_context, ArrayList<Exercise_Item> exerciseArrayList, OnNoteListener onNoteListener) {
+
+        this.exercise_context = exercise_context;
+        this.exerciseArrayList = exerciseArrayList;
+        this.onNoteListener = onNoteListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(exercise_context).inflate(R.layout.item_recommended_tile, parent, false);
+        return new ViewHolder(view, onNoteListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Exercise_Item exercise = exerciseArrayList.get(position);
+        holder.item_name.setText(exercise.getItem_name());
+        Picasso.get().load(exercise.getItem_image()).into(holder.item_image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseArrayList.size();
+    }
+    public void setExercises(ArrayList<Exercise_Item> exercises){
+        this.exerciseArrayList = exercises;
+        notifyDataSetChanged();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView item_name;
+        private ImageView item_image;
+        OnNoteListener onNoteListener;
+        public ViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
+            super(itemView);
+            item_name = itemView.findViewById(R.id.item_rec_name);
+            item_image = itemView.findViewById(R.id.item_rec_image);
+            this.onNoteListener = onNoteListener;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) { onNoteListener.onNoteClick(getAdapterPosition());}
+    }
+    public interface OnNoteListener{
+        void onNoteClick(int position);
+    }
+}
